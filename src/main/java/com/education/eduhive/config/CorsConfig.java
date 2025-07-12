aee51cbd4385cceb3633ab35d765de7c37991005
Firtness <u202122484@upc.edu.pe>
2025-07-12 06:37:36 -0500

    feat(cors fix): cors

--- src/main/java/com/education/eduhive/config/CorsConfig.java
+++ src/main/java/com/education/eduhive/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Apply CORS to all endpoints
-                .allowedOrigins("http://localhost:4200")  // Allow your Angular frontend
+                .allowedOrigins("https://frontend-web-applications-deploymen.vercel.app")  // Allow your Angular frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow cookies/auth (if needed)
